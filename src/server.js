780371ef3954fb76609ac26b082c6b55fdc7c35a
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2021-02-11 01:46:54 +0700

    TEST

--- src/server.js
+++ src/server.js
@@ -5,13 +5,15 @@ const router = express.Router();
 const app = express();
 
 const static_dir = path.join(__dirname, '../static');
+console.log(static_dir);
+console.log(path.join(static_dir, 'login.html'));
 
-router.get('/', (_req, res) => res.sendFile(path.join(static_dir, '/login.html')));
-router.get('/signin', (_req, res) => res.sendFile(path.join(static_dir + '/signin.html')));
-router.get('/chats', (_req, res) => res.sendFile(path.join(static_dir + '/chats.html')));
-router.get('/profile', (_req, res) => res.sendFile(path.join(static_dir + '/profile.html')));
-router.get('/500-error', (_req, res) => res.sendFile(path.join(static_dir + '/500-error.html')));
-router.get('/*', (_req, res) => res.sendFile(path.join(static_dir + '/404-error.html')));
+router.get('/', (_req, res) => res.sendFile(path.join(static_dir, 'login.html')));
+router.get('/signin', (_req, res) => res.sendFile(path.join(static_dir, 'signin.html')));
+router.get('/chats', (_req, res) => res.sendFile(path.join(static_dir, 'chats.html')));
+router.get('/profile', (_req, res) => res.sendFile(path.join(static_dir, 'profile.html')));
+router.get('/500-error', (_req, res) => res.sendFile(path.join(static_dir, '500-error.html')));
+router.get('/*', (_req, res) => res.sendFile(path.join(static_dir, '404-error.html')));
 
 app.use(express.static(static_dir));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
