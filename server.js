89d2c2bfc21cb255304c3318d5e24d7216c2845d
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2021-02-11 00:03:33 +0700

    no commets

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require('express');
-
+const serverless = require('serverless-http');
+const router = express.Router();
 const app = express();
-const PORT = 4000;
+
 const static_dir = __dirname + '/static';
 
+router.get('/', (_req, res) => res.sendFile(static_dir + '/login.html'));
+router.get('/signin', (_req, res) => res.sendFile(static_dir + '/signin.html'));
+router.get('/chats', (_req, res) => res.sendFile(static_dir + '/chats.html'));
+router.get('/profile', (_req, res) => res.sendFile(static_dir + '/profile.html'));
+router.get('/500-error', (_req, res) => res.sendFile(static_dir + '/500-error.html'));
+router.get('/*', (_req, res) => res.sendFile(static_dir + '/404-error.html'));
+
 app.use(express.static(static_dir));
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
-app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
-}); 
-
-app.get('/', (_req, res) => res.sendFile(static_dir + '/login.html'));
-
-app.get('/signin', (_req, res) => res.sendFile(static_dir + '/signin.html'))
-
-app.get('/chats', (_req, res) => res.sendFile(static_dir + '/chats.html'))
-
-app.get('/profile', (_req, res) => res.sendFile(static_dir + '/profile.html'))
-
-app.get('/500-error', (_req, res) => res.sendFile(static_dir + '/500-error.html'))
-
-app.get('/*', (_req, res) => res.sendFile(static_dir + '/404-error.html'))
+module.exports = app;
+module.exports.handler = serverless(app);
