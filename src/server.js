9f908633e718e856e2bdb02779c7344163f465a5
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2021-02-11 03:37:39 +0700

    TEST

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+'use strict';
 const express = require('express');
 const serverless = require('serverless-http');
 const path = require('path');
@@ -5,19 +6,21 @@ const router = express.Router();
 const app = express();
 
 //const static_dir = __dirname + '../static/';
-//console.log(static_dir);
+console.log(__dirname);
 //console.log(path.join(static_dir, 'login.html'));
 
-app.use('/', (_req, res) => res.sendFile(path.join(__dirname, '../static/login.html')));
-app.use('/signin', (_req, res) => res.sendFile(path.join(__dirname, '../static/signin.html')));
-app.use('/chats', (_req, res) => res.sendFile(path.join(__dirname, '../static/chats.html')));
-app.use('/profile', (_req, res) => res.sendFile(path.join(__dirname, '../static/profile.html')));
-app.use('/500-error', (_req, res) => res.sendFile(path.join(__dirname, '../static/500-error.html')));
-app.use('/*', (_req, res) => res.sendFile(path.join(__dirname, '../static/404-error.html')));
+const static_dir = { root: path.join(__dirname, '../static') };
 
-// app.use(express.static(static_dir));
+router.get('/', (_req, res) => res.sendFile('login.html', static_dir));
+router.get('/signin', (_req, res) => res.sendFile('signin.html', static_dir));
+router.get('/chats', (_req, res) => res.sendFile('chats.html', static_dir));
+router.get('/profile', (_req, res) => res.sendFile('profile.html', static_dir));
+router.get('/500-error', (_req, res) => res.sendFile('500-error.html', static_dir));
+router.get('/*', (_req, res) => res.sendFile('404-error.html', static_dir));
+
+//app.use(express.static(__dirname + '../static/'));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (_req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+// app.use('/', (_req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
