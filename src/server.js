e6fdf38d5482c648c821b89569a388916d1fd736
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2021-02-11 01:09:02 +0700

    TEST

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const path = require('path');
 const router = express.Router();
 const app = express();
 
-const static_dir = __dirname + '../static';
+const static_dir = path.join(__dirname, '../static');
 
+router.get('/', (_req, res) => res.sendFile(static_dir + '/login.html'));
 router.get('/signin', (_req, res) => res.sendFile(static_dir + '/signin.html'));
 router.get('/chats', (_req, res) => res.sendFile(static_dir + '/chats.html'));
 router.get('/profile', (_req, res) => res.sendFile(static_dir + '/profile.html'));
 router.get('/500-error', (_req, res) => res.sendFile(static_dir + '/500-error.html'));
 router.get('/*', (_req, res) => res.sendFile(static_dir + '/404-error.html'));
 
-// app.use(express.static(static_dir));
+app.use(express.static(static_dir));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
