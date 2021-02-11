69ddfec93674b39e3c9ee4a5bf8a5e114e2365e3
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2021-02-11 12:02:47 +0700

    test

---
+++ src/server.js
@@ -0,0 +1,20 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = 4000;
+const static_dir =  path.join(__dirname, '../static/');
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
+}); 
+
+app.use(express.static(static_dir));
+
+app.get('/', (_req, res) => res.sendFile(static_dir + 'login.html'));
+app.get('/signin', (_req, res) => res.sendFile(static_dir + 'signin.html'));
+app.get('/chats', (_req, res) => res.sendFile(static_dir + 'chats.html'));
+app.get('/profile', (_req, res) => res.sendFile(static_dir + 'profile.html'));
+app.get('/500-error', (_req, res) => res.sendFile(static_dir + '500-error.html'));
+app.get('/*', (_req, res) => res.sendFile(static_dir + '404-error.html'));
