a48d035e6a3237072c919aae4daee13e1fc20de4
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2021-02-11 10:19:18 +0700

    test

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-'use strict';
+/*'use strict';
 const express = require('express');
 const serverless = require('serverless-http');
 const path = require('path');
 const router = express.Router();
 const app = express();
-
-//const static_dir = __dirname + '../static/';
-console.log(__dirname);
-//console.log(path.join(static_dir, 'login.html'));
 
 const static_dir = { root: path.join(__dirname, '/static') };
 
@@ -18,9 +14,7 @@ router.get('/profile', (_req, res) => res.sendFile(path.resolve('static/profile.
 router.get('/500-error', (_req, res) => res.sendFile(path.resolve('static/500-error.html')));
 router.get('/*', (_req, res) => res.sendFile(path.resolve('static/404-error.html')));
 
-// app.use(express.static('static'));
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-// app.use('/', (_req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/.netlify/functions/server', router); 
 
 module.exports = app;
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app);*/
