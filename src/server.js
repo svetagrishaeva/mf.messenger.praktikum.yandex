019a1f4679355972e618a290df319926323a1408
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2021-02-11 13:00:35 +0700

    test

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const static_dir = path.join(__dirname, '../static/');
 app.use(express.static(static_dir));
 
 const router = express.Router();
-router.get('/', (_req, res) => res.sendFile(static_dir + 'login.html'));
+router.get('/login', (_req, res) => res.sendFile(static_dir + 'login.html'));
 router.get('/signin', (_req, res) => res.sendFile(static_dir + 'signin.html'));
 router.get('/chats', (_req, res) => res.sendFile(static_dir + 'chats.html'));
 router.get('/profile', (_req, res) => res.sendFile(static_dir + 'profile.html'));
@@ -15,3 +15,4 @@ router.get('/500-error', (_req, res) => res.sendFile(static_dir + '500-error.htm
 router.get('/*', (_req, res) => res.sendFile(static_dir + '404-error.html'));
 
 app.use('/', router);  
+app.use("/", (_req, res) => res.redirect("/login"));
