0852f4044972ff15747ee49a33648ad356124b20
Sveta Grishaeva <sveta.grishaeva.2013@mail.ru>
2021-03-06 19:55:19 +0700

    edit README.md + fix local build

--- src/pages/profile/templates.ts
+++ src/pages/profile/templates.ts
@@ -16,7 +16,7 @@ export const pageTmpl: string =
                 <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
             </svg>
         </div>
-        <a href="#openEditModal" class="circle-overlay" id="circle-overlay">
+        <a href="profile/#openEditModal" class="circle-overlay" id="circle-overlay">
             Поменять<br>аватар
         </a>
     </div>
@@ -45,9 +45,8 @@ export const pageTmpl: string =
     <div id="openEditModal" class="blackout">
         <div class="modalDialog">
             <h2>Загрузите файл</h2>
-            <a href="#" style="font-size: 18px;">Выберите файл на компьютере</a>
-
-            <a href="#" class="btn-confirm ">Поменять</a>
+            <a href="profile/#" style="font-size: 18px;">Выберите файл на компьютере</a>
+            <a href="profile/#" class="btn-confirm">Поменять</a>
         </div>
     </div>
  </div>`
